388f248d27a937c72f7578d6abc14a3cfca3b104
Kenneth Sumang <kennethsumang08@gmail.com>
2023-11-02 00:38:17 +0800

    refactor: put the subreddit list in another file

---
+++ app/_constants/reddit.constant.ts
@@ -0,0 +1,11 @@
+export const SUBREDDIT_LIST = [
+  'reddit',
+  'AskPH',
+  'OffMyChestPH',
+  'adultingph',
+  'phcareers',
+  'PHCreditCards',
+  'Philippines',
+  'phinvest',
+  'phmoneysaving',
+]
